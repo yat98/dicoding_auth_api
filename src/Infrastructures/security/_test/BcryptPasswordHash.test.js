4503af5b0aed2fa831fd1688e6826652b4a47027
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-10-09 16:14:41 +0800

    fix: test

--- src/Infrastructures/security/_test/BcryptPasswordHash.test.js
+++ src/Infrastructures/security/_test/BcryptPasswordHash.test.js
@@ -2,14 +2,16 @@ const bcrypt = require('bcrypt');
 const BcryptPasswordHash = require('../BcryptPasswordHash');
 
 describe('BcryptPasswordHash', () => {
-  it('should encrypt password correctly', async () => {
-    const spyHash = jest.spyOn(bcrypt, 'hash');
-    const bcryptPasswordHash = new BcryptPasswordHash(bcrypt);
-
-    const encryptedPassword = await bcryptPasswordHash.hash('plain_password');
-
-    expect(typeof encryptedPassword).toBe('string');
-    expect(encryptedPassword).not.toEqual('plain_password');
-    expect(spyHash).toBeCalledWith('plain_password', 10);
+  describe('hash function', () => {
+    it('should encrypt password correctly', async () => {
+      const spyHash = jest.spyOn(bcrypt, 'hash');
+      const bcryptPasswordHash = new BcryptPasswordHash(bcrypt);
+  
+      const encryptedPassword = await bcryptPasswordHash.hash('plain_password');
+  
+      expect(typeof encryptedPassword).toBe('string');
+      expect(encryptedPassword).not.toEqual('plain_password');
+      expect(spyHash).toBeCalledWith('plain_password', 10);
+    });
   });
 });
