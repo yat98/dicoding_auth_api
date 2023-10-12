a1591dc9983601ce26c3c2b065f47abcdf51c3db
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-10-12 10:59:33 +0800

    update: hello world

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -25,6 +25,7 @@ describe('HTTP server', () => {
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(200);
       expect(responseJson.value).toEqual('Hello world!');
+      expect(responseJson.value).toEqual('Hello world!');
     });
   });
 
